01163c87c10d05eb12aec694063c5faa335ee4f6
fengchuiguo1994 <huang182@live.cn>
2022-05-23 15:44:01 +0800

    Update InformationEntropyDifferentiallyExpressedGenes.py

--- InformationEntropyDifferentiallyExpressedGenes.py
+++ InformationEntropyDifferentiallyExpressedGenes.py
@@ -1,16 +1,58 @@
+import sys
 from math import log
+
+import gzip
+def readFile(infile):
+    """
+    infile: input file
+    return: file handle
+    """
+    if infile.endswith((".gz","gzip",".GZ",".GZIP")):
+        fin = gzip.open(infile,'rt')
+    else:
+        fin = open(infile,'r')
+    return fin
+
+def writeFile(outfile):
+    """
+    outfile: output file
+    return: file handle
+    """
+    if outfile.endswith((".gz","gzip",".GZ",".GZIP")):
+        fout = gzip.open(outfile,'wt')
+    else:
+        fout = open(outfile,'w')
+    return fout
 
 def calcShannonEnt(dataSet):
     shannonEnt = 0.0
     total = sum(dataSet)
     for k in dataSet:
         prob = k/total
-        shannonEnt -= prob * log(prob,2)
+        if prob != 0.0:
+            shannonEnt -= prob * log(prob,2)
     return shannonEnt
 
-dataSet = [8,8,8,8,8,8,8,8]
-print(log(8,2) - calcShannonEnt(dataSet))
-dataSet = [1,2,3,4,5,6,7,8]
-print(log(8,2) - calcShannonEnt(dataSet))
-dataSet = [1,1,1,1,1,1,1,8]
-print(log(8,2) - calcShannonEnt(dataSet))
+fin = readFile(sys.argv[1])
+fout = writeFile(sys.argv[2])
+
+for line in fin:
+    tmp = line.strip().split()
+    dataSet = []
+    aa = float(tmp[1])
+    flagSame = True
+    for i in tmp[1:]:
+        dataSet.append(float(i))
+        if aa != dataSet[-1]:
+            flagSame = False
+    if flagSame:
+        for i in range(len(dataSet)):
+            dataSet[i] = 1.0
+        fout.write("{0}\t{1:.4f}\n".format(line.strip(),log(len(dataSet),2) - calcShannonEnt(dataSet)))
+        # fout.write("{0}\t{1}\n".format(line.strip(),log(len(dataSet),2) - calcShannonEnt(dataSet)))
+    else:
+        fout.write("{0}\t{1:.4f}\n".format(line.strip(),log(len(dataSet),2) - calcShannonEnt(dataSet)))
+        # fout.write("{0}\t{1}\n".format(line.strip(),log(len(dataSet),2) - calcShannonEnt(dataSet)))
+    
+fin.close()
+fout.close()
