cd3792d393c2f43aa0e710970d82ed52e821009f
fengchuiguo1994 <huang182@live.cn>
2023-09-23 00:03:42 +0800

    Create catBam.py

---
+++ catBam.py
@@ -0,0 +1,34 @@
+import sys
+
+import pysam
+def readSam(insamfile):
+    if insamfile.endswith(".bam"):
+        insam = pysam.AlignmentFile(insamfile,'rb')
+    elif insamfile.endswith(".sam.gz"):
+        insam = pysam.AlignmentFile(insamfile,'rb')
+    elif insamfile.endswith(".sam"):
+        insam = pysam.AlignmentFile(insamfile,'r')
+    else:
+        raise ValueError("the input sam/bam file is not end with sam or bam!")
+    return insam
+        
+def writeSam(outsamfile,header):
+    if outsamfile.endswith(".bam"):
+        outsam = pysam.AlignmentFile(outsamfile,'wb',header=header)
+    elif outsamfile.endswith(".sam"):
+        outsam = pysam.AlignmentFile(outsamfile,'w',header=header)
+    else:
+        raise ValueError("the output sam/bam file is not end with sam or bam!")
+    return outsam
+
+samples = sys.argv[1].split(",")
+fin = readSam(samples[0])
+fout = writeSam(sys.argv[2],header=fin.header)
+fin.close()
+
+for sample in samples:
+    fin = readSam(sample)
+    for read in fin:
+        fout.write(read)
+    fin.close()
+fout.close()
