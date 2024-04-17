f150e7b420601339a2c5ba1ed3d7e5f4309c793e
fengchuiguo1994 <huang182@live.cn>
2024-04-17 22:53:19 +0800

    Update RColorSet.R

--- RColorSet.R
+++ RColorSet.R
@@ -1,6 +1,20 @@
 library(smallstuff)
+library(RColorBrewer)
 
-cols<-brewer.pal(9, "YlOrRd") # cols<-brewer.pal(3, "YlOrRd") 
+cols = brewer.pal(11,'RdYlBu')
+cols = brewer.pal(9, "YlOrRd") # cols<-brewer.pal(3, "YlOrRd") 
 pal<-colorRampPalette(cols)
 mycolors<-pal(16)
 plotCol(mycolors)
+color = colorRampPalette(c("#4DBBD5FF","#FF6F00FF","#E64B35FF"))(101)
+
+
+library(ggsci)
+cols = pal_npg()(10)
+
+
+library(scales)
+
+
+library(viridis)
+scale_color_viridis() + # 配合ggplot2
