64c92643042e968aa014857a038ef3e6be7a95b9
fengchuiguo1994 <huang182@live.cn>
2024-05-09 10:58:21 +0800

    Update ggalluvial.1.r

--- ggalluvial.1.r
+++ ggalluvial.1.r
@@ -34,3 +34,27 @@ ggplot(as.data.frame(HairEyeColor), aes(y = Freq,axis1 = Hair, axis2 = Eye, fill
   geom_text(stat = "stratum", size=3, aes(label = after_stat(stratum)), reverse = T) +
   scale_x_continuous(breaks = 1:3, expand = c(0,0), labels = c("Hair", "Eye", "Sex")) +
   ggtitle("Eye colors of 592 subjects, by sex and hair color")
+
+
+
+titanic_wide <- data.frame(Titanic)
+head(titanic_wide)
+#>   Class    Sex   Age Survived Freq
+#> 1   1st   Male Child       No    0
+#> 2   2nd   Male Child       No    0
+#> 3   3rd   Male Child       No   35
+#> 4  Crew   Male Child       No    0
+#> 5   1st Female Child       No    0
+#> 6   2nd Female Child       No    0
+ggplot(data = titanic_wide,
+       aes(axis1 = Class, axis2 = Sex, axis3 = Age,
+           y = Freq)) +
+  scale_x_discrete(limits = c("Class", "Sex", "Age"), expand = c(.2, .05)) +
+  xlab("Demographic") +
+  geom_alluvium(aes(fill = Survived)) +
+  geom_stratum() +
+  geom_text(stat = "stratum", aes(label = after_stat(stratum))) +
+  theme_minimal() +
+  ggtitle("passengers on the maiden voyage of the Titanic",
+          "stratified by demographics and survival")
+
