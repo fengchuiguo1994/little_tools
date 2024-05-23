3d160ea98ebd4a41661cc718f6635bd8e64bd869
fengchuiguo1994 <huang182@live.cn>
2024-05-23 14:07:59 +0800

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,7 @@
+## Python
+```
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+conda install -n base --override-channels -c conda-forge mamba 'python_abi=*=*cp*'
+```
+
