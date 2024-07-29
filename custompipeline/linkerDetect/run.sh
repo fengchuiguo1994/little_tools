b4d97eaf694302f6fcb5bf2c0cd9187336ce4360
fengchuiguo1994 <huang182@live.cn>
2024-07-29 15:55:46 +0800

    Create run.sh

---
+++ custompipeline/linkerDetect/run.sh
@@ -0,0 +1,5 @@
+https://github.com/4A69616E67/DHat
+java -cp /data/home/ruanlab/huangxingyu/Tools/DHat-master/out/artifacts/DHat_jar/DHat.jar Component.tool.LinkerDetection -i ../LHH0135_R1.fq.gz -e no 
+Unknown enzyme
+AGAGTCAGATAAGATATCGCGTCT	+	378.0
+AGACGCGATATCTTATCTGACTCT	+	501.0
